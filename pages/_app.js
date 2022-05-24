a4479228fedaf31592d834a6d10fba8b8f7eccbc
Emmanuel Tolani <emmanuel@Emmanuels-MacBook-Air.local>
2022-05-24 14:37:47 -0500

    added overflow hidden

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import "../styles/styles.scss";
 import React from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Head from "next/head";
 gsap.registerPlugin(ScrollTrigger);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <Head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+  </Head>
+  <Component {...pageProps} />;
+  </>
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,6 @@ class MyDocument extends Document {
             crossOrigin="true"
             referrerPolicy="no-referrer"
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
